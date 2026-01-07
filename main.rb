11ceafd840d7e4cf1152b27fe39b5210ac6475cc
Gness Erquint <gness.na@gmail.com>
2026-01-07 21:27:42 +0300

    Implemented client spec namespacing as `[Client:Name]`. `[Client:All] reserved as client spec terminator, signalling the beginning of parameter order spec. Redundant order spec parameters skipped gracefully. Implemented path validation script arguments. Migrated File and Dir class method usage to Pathname instance methods. Retired `Dir.chdir(__dir__)` prototyping aid. Iterator parameter identifiers made informative. Sundry improvements.

--- main.rb
+++ main.rb
@@ -1,7 +1,6 @@
 # encoding: BINARY
 Encoding.default_external = Encoding::BINARY
 Encoding.default_internal = Encoding::BINARY
-Dir.chdir(__dir__)
 
 require 'pathname'
 
@@ -29,79 +28,78 @@ class Hash
   end
 end
 
-usage_hint = 'Usage: ruby main.rb <SPEC_DIR> <CONFIGS_DIR>'
-
-abort usage_hint unless ARGV.size == 2
-
+abort 'Usage: ruby main.rb <SPEC_DIR> <CONFIGS_DIR>' unless ARGV.size == 2
 spec_path = Pathname.new(ARGV[0]).cleanpath
+abort "File #{spec_path} not found!" unless spec_path.exist?
 configs_path = Pathname.new(ARGV[1]).cleanpath
+abort "Directory #{configs_path} not found!" unless configs_path.exist?
 enriched_path = configs_path.join('./enriched/').cleanpath
-Dir.mkdir(enriched_path) rescue Errno::EEXIST
-configs_path_glob = configs_path.join('./*.conf').cleanpath
+enriched_path.mkdir rescue Errno::EEXIST
 client_args = Hash.new
 spec_order = Array.new
 
-File.open(spec_path, ?r) do |file|
+spec_path.open(?r) do |spec_file|
   current_client = nil
   current_section = nil
   
-  until (line = file.readline(chomp: true)) == '###' do
-    if match_data = line.match(/!(\w+)!/) then
+  spec_file.each_line(chomp: true) do |client_spec_line|
+    if match_data = client_spec_line.match(/\[Client:(\w+)\]/) then
       current_client = match_data.captures[0]
+      break if current_client == 'All'
       client_args[current_client] ||= Hash.new
-    elsif match_data = line.match(/\[(\w+)\]/) then
+    elsif match_data = client_spec_line.match(/\[(\w+)\]/) then
       current_section = match_data.captures[0]
       client_args[current_client][current_section] ||= Hash.new
-    elsif match_data = line.match(/(\w+) ?= ?(.+)/) then
+    elsif match_data = client_spec_line.match(/(\w+) ?= ?(.+)/) then
       parameter, argument = match_data.captures
       client_args[current_client][current_section][parameter] = argument
     end
   end
   
-  spec_order = file.each_line.map(&:chomp).to_a
+  spec_order = spec_file.each_line.map(&:chomp).to_a
 end
 
-Dir.glob(configs_path_glob) do |file_path|
-  File.open(file_path, ?r) do |file|
+configs_path.glob('./*.conf') do |file_path|
+  file_path.open(?r) do |config_file|
     pre_config_string = String.new
     config_hash = Hash.new
     current_section = nil
     
-    file.each_line(chomp: true) do |line|
-      if section_match_data = line.match(/\[(\w+)\]/) then
+    config_file.each_line(chomp: true) do |config_line|
+      if section_match_data = config_line.match(/\[(\w+)\]/) then
         section_title = section_match_data.captures[0]
         config_hash[section_title] ||= Hash.new
         current_section = section_title
-      elsif parameter_match_data = line.match(/(\w+) ?= ?(.+)/) then
+      elsif parameter_match_data = config_line.match(/(\w+) ?= ?(.+)/) then
         abort "Malformed input #{file_path}: parameter outside of sections!" unless current_section
         parameter, argument = parameter_match_data.captures
         config_hash[current_section][parameter] = argument
       elsif current_section.nil? then
-        pre_config_string << "#{line}\n"
+        pre_config_string << "#{config_line}\n"
       end
     end
     
-    client_args.keys.each do |client|
-      config_hash.merge!(client_args[client], true)
+    client_args.keys.each do |client_title|
+      config_hash.merge!(client_args[client_title], true)
       config_string = pre_config_string.dup
       
-      spec_order.each do |line|
-        if section_match_data = line.match(/\[(\w+)\]/) then
+      spec_order.each do |order_spec_line|
+        if section_match_data = order_spec_line.match(/\[(\w+)\]/) then
           current_section = section_match_data.captures[0]
           config_string << "[#{current_section}]\n"
-        elsif line.empty? then
+        elsif order_spec_line.empty? then
           config_string << ?\n
         else
-          argument = config_hash[current_section][line]
-          abort "Spec order references undefined parameter: #{current_section}.#{line}" unless argument
-          config_string << "#{line} = #{argument}\n"
+          argument = config_hash[current_section][order_spec_line]
+          next nil unless argument
+          config_string << "#{order_spec_line} = #{argument}\n"
         end
       end
       
-      enriched_client_path = enriched_path.join(client).cleanpath
-      Dir.mkdir(enriched_client_path) rescue Errno::EEXIST
-      enriched_filename = enriched_client_path.join(File.basename(file_path)).cleanpath
-      File.binwrite(enriched_filename, config_string)
+      enriched_client_path = enriched_path.join(client_title).cleanpath
+      enriched_client_path.mkdir rescue Errno::EEXIST
+      enriched_filename = enriched_client_path.join(file_path.basename).cleanpath
+      enriched_filename.binwrite(config_string)
       puts("Wrote #{enriched_filename}")
     end
     
